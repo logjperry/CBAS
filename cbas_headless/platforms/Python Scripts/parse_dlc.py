31fec4a4b4bb8b7056b4e6a75d61ee3e211b21c4
Jason <jrtage@gmail.com>
2023-11-30 11:12:36 -0600

    finished dist func added speed func

--- cbas_headless/platforms/Python Scripts/parse_dlc.py
+++ cbas_headless/platforms/Python Scripts/parse_dlc.py
@@ -1,8 +1,3 @@
-#Calculate distance from bodypart to object as timeseries
-#Mutliply likelyhood of two points together to get a general likelyhood for the distance
-#List of lists for distance of every point on the mouse to fixed reference point
-#Fixed ref points will be hardcoded list for now, will end up being user input that matches header names
-
 #Build dict of numpy arr
 #{
 #nose:{x:[...],y:[...]}
@@ -12,13 +7,32 @@
 import pandas as pd
 import numpy as np
 
+#Builds dictionary similar to distances func but calculates speed of each part to or from
+#each fixed point between the prev and current frame. Requires timeframe in seconds (10fps = 0.1)
+def speed(distDict, timeframe):
+    speedDict = {}
+    for point in distDict:
+        speedDict[point] = {}
+        for part in distDict[point]:
+            speedDict[point][part] = [
+                (distDict[point][part][i] - distDict[point][part][i -1]) / timeframe 
+                if i > 0 else 0 
+                for i in range(len(distDict[point][part]))
+            ]
+            
+    return speedDict
+
+#Build dict of distances from body parts to fixed points
 def distances(df, parts, fixedPoints):
     pointsDict = {}
     for point in fixedPoints:
-        #Need unique part here for fixed point for body part to reference
-        pointsDict[point] = []
+        uniquePoint = df.loc[:,point]
+        pointsDict[point] = {}
         for part in parts:
-            uniquePart = df.loc[:,part]#this will reference outer for loop unique part, not master df CHANGE THIS
+            uniquePart = df.loc[:,part]
+            pointsDict[point][part] =  np.sqrt((uniquePoint['x'] - uniquePart['x'])**2 + (uniquePoint['y'] - uniquePoint['y'])**2)
+    return pointsDict
+
 
 #add column with velocity of prev to current frame
 def velocity(df, points):
@@ -36,18 +50,21 @@ def main():
     df.drop(('bodyparts', 'coords'), axis= 1, inplace=True)
     
     points = []
-    fixedPoints = ['nest', 'spout', 'food_hopper']
-    bodyParts = ['nose', 'hand_left', 'hand_right', 'back', 'base_tail']
-    
-
     for col in df.columns:
         if col[0] not in points:
             points.append(col[0])
 
+
+    fixedPoints = ['nest', 'spout', 'food_hopper']
+    bodyParts = ['nose', 'hand_left', 'hand_right', 'back', 'base_tail']
+    
+
     velocity(df, points)
     
-    distances = distances(df, bodyParts, fixedPoints)
-
+    distDict = distances(df, bodyParts, fixedPoints)
+    speedDict = speed(distDict, 0.1)
+    print(distDict)
+    
     
 
     
